2e1e2916db469d19fcc087880f5d10cca24b98d8
Spotika <andrey.a.cherkashin@gmail.com>
2023-11-07 00:18:32 +0300

    added user model

---
+++ src/db/oid.py
@@ -0,0 +1,13 @@
+class OID(str):
+    """Расширенный класс ObjectId c возможностью валидации"""
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if v == '':
+            raise TypeError('ObjectId is empty')
+        if ObjectId.is_valid(v) is False:
+            raise TypeError('ObjectId invalid')
+        return str(v)
