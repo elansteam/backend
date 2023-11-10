86cee96ece3953ff6cc111a5a09da0354fc41682
Mark Fomin <mark@difhel.dev>
2023-11-10 19:59:43 +0300

    refactor: replacing of deprecated `on_event` mechanism with lifespan

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from config import Config
 from db.abstract_database_manager import AbstractDatabaseManager
@@ -5,20 +6,25 @@ import routers.users
 import routers.auth
 import routers.roles
 
-app = FastAPI(title=Config.app_title, debug=True)
+
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
+    """Application lifespan (see https://fastapi.tiangolo.com/advanced/events/)
+    In the application lifespan we need to connect and close connection to
+    the database.
+    
+    Args:
+        _app (FastAPI): application object. It is not using right now
+    """
+    # on startup
+    AbstractDatabaseManager.connect_to_database(path=Config.db_path)
+    yield
+    # on shutdown
+    AbstractDatabaseManager.close_database_connection()
+
+
+app = FastAPI(title=Config.app_title, debug=True, lifespan=lifespan)
 
 app.include_router(routers.users.router, prefix="/api/users")
 app.include_router(routers.auth.router, prefix="/auth")
 app.include_router(routers.roles.router, prefix="/api/roles")
-
-
-@app.on_event("startup")
-async def startup():
-    """Старт"""
-    AbstractDatabaseManager.connect_to_database(path=Config.db_path)
-
-
-@app.on_event("shutdown")
-async def shutdown():
-    """Конец"""
-    AbstractDatabaseManager.close_database_connection()
