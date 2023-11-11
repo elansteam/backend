4ffde3186321f6d8818988c50ba63044254213a3
Spotika <andrey.a.cherkashin@gmail.com>
2023-11-11 12:30:54 +0300

    added templates for error responces but unused yet

---
+++ src/utils/utils.py
@@ -0,0 +1,19 @@
+from starlette.responses import JSONResponse
+
+
+def get_error_schema(description: str, msg: str = "error msg"):
+    return {"description": description,
+            "content": {
+                "application/json": {
+                    "example": {"detail": [{
+                        "msg": msg,
+                    }]},
+                }
+            }}
+
+
+def get_error_response(msg: str, status_code: int = 400):
+    """Сообщение об ошибке"""
+    return JSONResponse(status_code=status_code, content={"detail": [
+        {"msg": msg}
+    ]})
