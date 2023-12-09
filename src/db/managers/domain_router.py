453f724300476521c114d334989f23b3de638004
Spotika <andrey.a.cherkashin@gmail.com>
2023-12-09 23:31:07 +0300

    added not implemented interface for DomainRouter

--- src/db/managers/domain_router.py
+++ src/db/managers/domain_router.py
@@ -10,4 +10,21 @@ class DomainRouter(AbstractDatabaseManager):
 
     collection_name = Config.Collections.domain_router
 
-    def get_domain_node(self): ...
+    def get_domain_node(self, target_name: str) -> DomainNode | None:
+        """
+        Getting the domain node by target object name
+        Args:
+            target_name: target object name
+        Returns:
+            DomainNode or None of not found
+        """
+        raise NotImplementedError  # FIXME
+
+    def raname_domain_node(self, target_name: str, replace_name: str) -> None:
+        """
+        Rename domain node
+        Args:
+            target_name: target object name
+            replace_name: replacing name
+        """
+        raise NotImplementedError  # FIXME
