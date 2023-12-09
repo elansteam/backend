2a1d591352b5b9a3b52b726ce885e9885eb2df6f
Spotika <andrey.a.cherkashin@gmail.com>
2023-12-10 00:09:56 +0300

    added a new node router methods

--- src/db/managers/domain_router.py
+++ src/db/managers/domain_router.py
@@ -10,7 +10,18 @@ class DomainRouter(AbstractDatabaseManager):
 
     collection_name = Config.Collections.domain_router
 
-    def get_domain_node(self, target_name: str) -> DomainNode | None:
+    async def get_by_id(self, _id: ObjectId) -> DomainNode | None:
+        """
+        Getting the domain node by id
+        Args:
+            _id: domain node id
+
+        Returns:
+            DomainNode or None of not found
+        """
+        raise NotImplementedError  # FIXME
+
+    async def get_by_target_name(self, target_name: str) -> DomainNode | None:
         """
         Getting the domain node by target object name
         Args:
@@ -20,11 +31,34 @@ class DomainRouter(AbstractDatabaseManager):
         """
         raise NotImplementedError  # FIXME
 
-    def raname_domain_node(self, target_name: str, replace_name: str) -> None:
+    async def get_by_target_id(self, target_id: ObjectId) -> DomainNode | None:
         """
-        Rename domain node
+        Getting the domain node by target object id
         Args:
-            target_name: target object name
-            replace_name: replacing name
+            target_id: target object id
+        Returns:
+            DomainNode or None of not found
+        """
+        raise NotImplementedError  # FIXME
+
+    async def rename_target(self, _id: ObjectId, new_name: str) -> bool:
+        """
+        Renaming node target by own node id
+        Args:
+            _id: domain node id
+            new_name: new node id
+        Returns:
+            True if the node was renamed successfully, else False
+        """
+        raise NotImplementedError  # FIXME
+
+    async def create(self, domain_node: DomainNode) -> bool:
+        """
+        Creating a new domain node
+        Args:
+            domain_node: node to create
+
+        Returns:
+            True if the object was created successfully, else False
         """
         raise NotImplementedError  # FIXME
