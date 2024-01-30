fa471d79461b25ee2cf2c2215d3286c79cab0073
Spotika <andrey.a.cherkashin@gmail.com>
2024-01-30 16:31:51 +0300

    feat: add role database manager

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -2,7 +2,9 @@
 from src.db.managers.user_database_manager import UserDatabaseManager
 from src.db.managers.group_database_manager import GroupDatabaseManager
 from src.db.managers.domain_router_database_manager import DomainRouterDatabaseManager
+from src.db.managers.role_database_manager import RoleDatabaseManager
 
-user = UserDatabaseManager()
-group = GroupDatabaseManager()
-domain = DomainRouterDatabaseManager()
+user: UserDatabaseManager = UserDatabaseManager()
+group: GroupDatabaseManager = GroupDatabaseManager()
+domain: DomainRouterDatabaseManager = DomainRouterDatabaseManager()
+role: RoleDatabaseManager = RoleDatabaseManager()
---
+++ src/db/managers/role_database_manager.py
@@ -0,0 +1,32 @@
+"""Role database manager."""
+
+from src.db.helpers.abstract_database_manager import AbstractDatabaseManager
+from src.db.models.role import Role
+from src.config import Config
+
+
+class RoleDatabaseManager(AbstractDatabaseManager):
+    """Role database manager."""
+
+    collection_name = Config.Collections.roles
+
+    async def insert(self, role: Role) -> None:
+        """
+        Insert new role to database
+        Args:
+            role: role to insert
+        """
+        await self.collection.insert_one(role.model_dump(by_alias=True))
+
+    async def get(self, role_id: str) -> Role | None:
+        """
+        Get role by id
+        Args:
+            role_id: Target role id
+        Returns:
+            Role or None
+        """
+        result = await self.collection.find_one({"_id": role_id})
+        if result is None:
+            return None
+        return Role(**result)
