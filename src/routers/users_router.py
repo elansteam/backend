d5a1b33470cb2c8696d781eb2a3476ad71ed5eea
Spotika <andrey.a.cherkashin@gmail.com>
2024-01-31 13:46:03 +0300

    feat(routers): add role <add_role> method

--- src/routers/users_router.py
+++ src/routers/users_router.py
@@ -8,43 +8,12 @@ from utils.response_utils import get_error_response, get_response_model, get_err
 from auth.utils import auth_user
 from auth.utils import Permissions
 
-
 router = APIRouter()
 
 
-# @router.post(
-#     "/add_role",
-#     response_model=Role,
-#     responses={
-#         400: get_error_schema("Failed to add role to user")
-#     }
-# )
-# async def add_role_to_user(user_id: int, role_id: str,
-#                            _current_user: User = Depends(auth_user(
-#                                Permissions.CAN_ADD_ROLE_TO_USER
-#                            ))):
-#     """Adding role to user"""
-#     user_to_add = await db.user.get(user_id)
-#
-#     if user_to_add is None:
-#         return get_error_response(f"User with id {user_id} doesn't exist")
-#
-#     role = await db.role.get(role_id)
-#
-#     if role is None:
-#         return get_error_response(f"Role with id <{role_id}> doesn't exist")
-#
-#     if role_id in user_to_add.roles:
-#         return get_error_response(
-#             f"Role with id <{role_id}> already exists for user with id <{user_id}>"
-#         )
-#
-#     await db.user.add_role(user_id, role_id)
-#     return role
-
 @router.post(
     "/add_role",
-    response_model=get_response_model(dict[str, None]),
+    response_model=get_response_model(),
     responses={
         400: get_error_schema("Failed to add role")
     }
@@ -68,3 +37,5 @@ async def add_role(user_id: int, role_id: str,
         return get_error_response("ROLE_ALREADY_EXISTS")
 
     await db.user.add_role(user_id, role_id)
+
+    return get_response()
