08cad759a2f7d837603bc2b802b455bdad5874fb
Spotika <andrey.a.cherkashin@gmail.com>
2024-02-05 13:54:29 +0300

    refactor: changed email regex

--- src/db/models/annotations/validators.py
+++ src/db/models/annotations/validators.py
@@ -12,12 +12,7 @@ def email_string(email: str) -> str:
     Raises:
         ValueError: If string is not a valid email
     """
-    pattern = (r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:"
-               r"[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e"
-               r"-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a"
-               r"-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2("
-               r"5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08"
-               r"\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])")
+    pattern = r"^[a-zA-Z0-9_\.]+@[a-zA-Z0-9_\.]+\.[a-z]{2,5}"
     match = re.match(pattern, email) is not None
     if not match:
         raise ValueError(f"String {email} is not a valid email")
