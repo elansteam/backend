f249bd5db9bd383ef10b07a9a47b681b3ef5ff00
Spotika <andrey.a.cherkashin@gmail.com>
2024-02-12 20:20:07 +0300

    feat: added more tasks methods

--- src/routers/tasks_router.py
+++ src/routers/tasks_router.py
@@ -3,11 +3,11 @@ from typing import Any
 
 from fastapi import APIRouter, Depends, UploadFile
 from pydantic import BaseModel
+from starlette.responses import FileResponse
 
 from auth.utils import auth_user
 from utils.response_utils import get_error_response, get_response, get_response_model, \
     get_error_schema
-from db.models.contest import Contest, ContestToCreate
 from db.models.task import Task
 from db.models.user import User
 import db
@@ -45,3 +45,87 @@ async def create_task(name: NameAnnotation, task_archive: UploadFile, _current_u
     created_task_id = await db.task.insert_with_id(task_to_create)
     response = AddTask(created_task_id=created_task_id)
     return get_response(response)
+
+
+@router.get(
+    "/get_legend"
+)
+async def get_task_legend(_id: IntIdAnnotation, _current_user: User = Depends(auth_user())):
+    """Retreive task legend"""
+
+    try:
+        to_return = FileResponse(path=f'./data/problems/{_id}/legend.mdx', filename="legend.mdx",
+                                 media_type='multipart/form-data')
+        return to_return
+    except Exception:
+        return get_error_response("FILE_NOT_FOUND")
+
+
+@router.get(
+    "/get_input"
+)
+async def get_task_input(_id: IntIdAnnotation, _current_user: User = Depends(auth_user())):
+    """Retreive task legend"""
+
+    try:
+        to_return = FileResponse(path=f'./data/problems/{_id}/input.mdx', filename="input.mdx",
+                                 media_type='multipart/form-data')
+        return to_return
+    except Exception:
+        return get_error_response("FILE_NOT_FOUND")
+
+
+@router.get(
+    "/get_output"
+)
+async def get_task_output(_id: IntIdAnnotation, _current_user: User = Depends(auth_user())):
+    """Retreive task legend"""
+
+    try:
+        to_return = FileResponse(path=f'./data/problems/{_id}/output.mdx', filename="output.mdx",
+                                 media_type='multipart/form-data')
+        return to_return
+    except Exception:
+        return get_error_response("FILE_NOT_FOUND")
+
+
+@router.get(
+    "/get_scoring"
+)
+async def get_task_scoring(_id: IntIdAnnotation, _current_user: User = Depends(auth_user())):
+    """Retreive task legend"""
+
+    try:
+        to_return = FileResponse(path=f'./data/problems/{_id}/scoring.mdx', filename="scoring.mdx",
+                                 media_type='multipart/form-data')
+        return to_return
+    except Exception:
+        return get_error_response("FILE_NOT_FOUND")
+
+
+@router.get(
+    "/get_notes"
+)
+async def get_notes(_id: IntIdAnnotation, _current_user: User = Depends(auth_user())):
+    """Retreive task legend"""
+
+    try:
+        to_return = FileResponse(path=f'./data/problems/{_id}/notes.mdx', filename="notes.mdx",
+                                 media_type='multipart/form-data')
+        return to_return
+    except Exception:
+        return get_error_response("FILE_NOT_FOUND")
+
+
+@router.get(
+    "/get_name"
+)
+async def get_name(_id: IntIdAnnotation, _current_user: User = Depends(auth_user())) -> Any:
+    """Retreive task name by task id"""
+
+    task = await db.task.get(_id)
+
+    if task is None:
+        return get_error_response("TASK_NOT_FOUND")
+
+    return task.name
