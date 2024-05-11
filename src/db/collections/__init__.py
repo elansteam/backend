de904f070015638661dcb91723f2900dcab2bd50
Spotika <andrey.a.cherkashin@gmail.com>
2024-05-11 11:42:57 +0300

    fix: improve type hints x2

--- src/db/collections/__init__.py
+++ src/db/collections/__init__.py
@@ -1,14 +1,14 @@
 """All collections got by already configuring names."""
 
-from db import db
+from db import db as _db
 from config import config as _config  # to improve type hints
 
-users = db.get_collection(_config.database.collections.users)
-domains = db.get_collection(_config.database.collections.domains)
-group_roles = db.get_collection(_config.database.collections.group_roles)
-groups = db.get_collection(_config.database.collections.groups)
-internal_counters = db.get_collection(_config.database.collections.internal_counters)
-roles = db.get_collection(_config.database.collections.roles)
+users = _db.get_collection(_config.database.collections.users)
+domains = _db.get_collection(_config.database.collections.domains)
+group_roles = _db.get_collection(_config.database.collections.group_roles)
+groups = _db.get_collection(_config.database.collections.groups)
+internal_counters = _db.get_collection(_config.database.collections.internal_counters)
+roles = _db.get_collection(_config.database.collections.roles)
 
 __all__ = [
     "users",
