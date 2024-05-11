ca940396a70aa1b81518275040fa2222497b07a8
Spotika <andrey.a.cherkashin@gmail.com>
2024-05-11 11:25:33 +0300

    refactor: refactor user base model

--- src/db/models/user.py
+++ src/db/models/user.py
@@ -7,11 +7,11 @@ from db.annotations import IntIdAnnotation, DomainAnnotation, EmailAnnotation, \
 class User(BaseModel):
     """User representation in database"""
     id: IntIdAnnotation = Field(alias='_id')
-    email: EmailAnnotation
     domain: DomainAnnotation | None = Field(None)
     password_hash: str
     first_name: str
     last_name: str
     mid_name: str | None = Field(None)
+    groups: list[IntIdAnnotation] = Field([])
     roles: list[StrIdAnnotation] = Field([])
-    """List of global roles, which user have"""
+    email: EmailAnnotation | None
