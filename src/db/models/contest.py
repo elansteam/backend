e5207cf391000248d4b1856f6d654ebe0d02da45
Spotika <andrey.a.cherkashin@gmail.com>
2024-05-11 12:37:24 +0300

    refactor: refactor contest base model

--- src/db/models/contest.py
+++ src/db/models/contest.py
@@ -1,27 +1,22 @@
 """Group definition"""
+import datetime
 from pydantic import BaseModel, Field
-from db.models.submission import Submission
 from db.annotations import IntIdAnnotation, NameAnnotation, DescriptionAnnotation, \
     DomainAnnotation
 
 
 class Contest(BaseModel):
-    """Group representation in database"""
+    """https://app.clickup.com/9015604104/v/dc/8cnycw8-115"""
+
     id: IntIdAnnotation = Field(alias='_id')
-    """Group id"""
-
     name: NameAnnotation
-    """Group name"""
-
-    description: DescriptionAnnotation = Field("")
-    """Group description"""
-
     domain: DomainAnnotation | None = Field(None)
-    """Group domain"""
-
-    linked_group: IntIdAnnotation
-    """Linked group"""
-
-    problems: list[IntIdAnnotation] = Field([])
-
-    submissions: list[Submission] = Field([])
+    local_domain: DomainAnnotation | None = Field(None)
+    description: DescriptionAnnotation = Field("")
+    submissions: list[IntIdAnnotation]
+    members: list[IntIdAnnotation]
+    creators: list[IntIdAnnotation]
+    begin_time: datetime.datetime
+    duration: datetime.datetime
+    after_work: bool
+    group_id: IntIdAnnotation
