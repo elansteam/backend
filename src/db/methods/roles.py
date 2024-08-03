266a66fef04887d17da6d550d75b7f8f1fa726c0
Spotika <andrew.a.cherkashin@gmail.com>
2024-08-03 23:54:22 +0200

    refactor: fix pylint

--- src/db/methods/roles.py
+++ src/db/methods/roles.py
@@ -2,4 +2,8 @@ from db.types import Role
 from .collections import roles
 
 def get(role_name: str) -> Role | None:
-    roles.find_one({"_id": role_name})
+    role = roles.find_one({"_id": role_name})
+
+    if role is None:
+        return None
+    return Role(**role)
