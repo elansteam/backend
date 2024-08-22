22ec6c9a3f62df2294f6f114071cbac4b74e273d
Spotika <andrey.a.cherkashin@gmail.com>
2024-08-22 15:34:42 +0300

    refactor: remove unnecessary user info

--- src/db/types/user.py
+++ src/db/types/user.py
@@ -6,8 +6,6 @@ from utils.schemas import BaseModel
 class _UserBase(BaseModel):
     domain: str | None = None
     first_name: str
-    last_name: str | None = None
-    mid_name: str | None = None
     groups: list[int] = []
     roles: list[str] = []
     hashed_password: str
