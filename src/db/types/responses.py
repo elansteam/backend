0d73c286954507999765b3a6e897edfd5d921ea5
Spotika <andrey.a.cherkashin@gmail.com>
2024-09-04 23:30:48 +0300

    feat(types): response types

--- src/db/types/responses.py
+++ src/db/types/responses.py
@@ -1,6 +1,8 @@
 # pylint: disable=invalid-name
 from __future__ import annotations
+
 from utils.schemas import BaseModel
+from db.types import types
 
 
 class RS:
@@ -16,7 +18,12 @@ class RS:
             id: int
             first_name: str
             email: str
+        class get_orgs(BaseModel):
+            organizations: list[types.Organization]
     class test:
         class signup(BaseModel):
             access_token: str
             refresh_token: str
+        class orgs:
+            class create(BaseModel):
+                organization: types.Organization
