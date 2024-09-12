d912297f7e3ca741bc16ba8144b8560127156f1c
Spotika <andrey.a.cherkashin@gmail.com>
2024-09-12 21:41:41 +0300

    chore: add sessions to all db methods

--- src/db/methods/users.py
+++ src/db/methods/users.py
@@ -1,28 +1,27 @@
 from pymongo.errors import DuplicateKeyError
+from pymongo.client_session import ClientSession
 
 from db.types import types
 from .collections import users
 from .helpers import insert_with_auto_increment_id
 
 
-def get(user_id: int) -> types.User | None:
-    if (user := users.find_one({"_id": user_id})) is None:
+def get(user_id: int, session: ClientSession | None = None) -> types.User | None:
+    if (user := users.find_one({"_id": user_id}, session=session)) is None:
         return None
     return types.User(**user)
 
-def get_by_email(email: str):
-    if (user := users.find_one({"email": email})) is None:
+def get_by_email(email: str, session: ClientSession | None = None):
+    if (user := users.find_one({"email": email}, session=session)) is None:
         return None
     return types.User(**user)
 
-def insert_user_with_id(user: types.UserWithoutID) -> int | None:
+def insert_user_with_id(user: types.UserWithoutID, session: ClientSession | None = None) -> int | None:
     """
     Returns:
         Inserted user id or None, if error occurred
     """
     try:
-        return insert_with_auto_increment_id(
-            users, user.db_dump()
-        )
+        return insert_with_auto_increment_id(users, user.db_dump(), session=session)
     except DuplicateKeyError:
         return None
