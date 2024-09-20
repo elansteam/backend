96666cd6993d9b8d86b4588f02c26a1e344ba2ff
Spotika <andrey.a.cherkashin@gmail.com>
2024-09-20 09:36:50 +0300

    fix: fix response serialize

--- src/utils/schemas.py
+++ src/utils/schemas.py
@@ -1,12 +1,8 @@
 from typing import Any
 import pydantic
-import pydantic.decorator
 
 
 class BaseModel(pydantic.BaseModel):
-    def model_dump(self, by_alias=True, **kwargs) -> dict[str, Any]:
-        return super().model_dump(by_alias=by_alias, **kwargs)
-
     def db_dump(self, by_alias=True, **kwargs) -> dict[str, Any]:
         result = self.model_dump(by_alias=by_alias, **kwargs)
         if "id" in result:
